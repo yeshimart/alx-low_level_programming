2c075b1886b611af3ddf8b6339b396b019a3d151
Yeshitla Tsegaw <yeshitla12345@gmail.com>
2023-06-16 05:55:12 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
-}
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <time.h>
 
+	/**
+	 * main - Will store a different value every time you will run this program
+	 *
+	 * Return: Always 0 (Success)
+	 */
+		int main(void)
+	{
+		int n;
+
+        	srand(time(0));
+        	n = rand() - RAND_MAX / 2;
+	       	printf("%d ", n);
+		if (n > 0)
+        	{
+                	printf("is positive\n");
+        	}
+        	else if (n == 0)
+        	{
+                	printf("is zero\n");
+        	}
+        	else
+       		chmod a+x * 0-positive_or_negative.cprintf("is negative\n");
+        	}
+		return (0);
+	}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
