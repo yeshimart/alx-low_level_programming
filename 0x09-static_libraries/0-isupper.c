ec068a67742ac63041617dbc7158beab2e7006bd
Yeshitla Tsegaw <yeshitla12345@gmail.com>
2023-07-07 14:06:08 -0700

    create_static_lib.sh

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _isupper - uppercase letters
- * @c: char to check
+ * _isupper - checks if a character is an uppercase letter
+ * @c: the character to check
  *
- * Return: 0 or 1
+ * Return: 1 if @c is an uppercase letter, 0 otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else;
-		return (0);
+		return (1); /* Return 1 if @c is an uppercase letter */
+	else
+		return (0); /* Return 0 if @c is not an uppercase letter */
 }
